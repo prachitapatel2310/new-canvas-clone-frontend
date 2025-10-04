75429abd876a9555a783bfa42d4d1f5b25770552
Vishwapujara <vishwapujara.08@gmail.com>
2025-10-04 16:41:32 -0400

    lab 2.1

---
+++ app/Labs/Lab2/Float.tsx
@@ -0,0 +1,49 @@
+import "./index.css";
+export default function Float() {
+    return (
+        <div id="wd-float-divs">
+            <h2>Float</h2>
+            <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni ipsa possimus aliquid et dolorum, dignissimos, alias a illo inventore earum pariatur ab dolorem. Saepe, ratione a voluptatum dolorum eligendi iure.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae temporibus impedit eos aperiam quod ea ipsa voluptatum, iste corporis dicta est error. Molestias harum voluptates perspiciatis impedit amet, sit dolores.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius incidunt quidem voluptatem maiores, laudantium fugiat modi, sit quas laboriosam eveniet ex animi ipsam assumenda soluta quisquam blanditiis delectus aliquam neque?
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, fugit aliquid nulla atque, ducimus repellendus ipsum voluptates placeat id consectetur dicta maxime distinctio necessitatibus eius unde. Fugiat asperiores eos excepturi.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, eius ipsum, obcaecati quibusdam earum maxime sit repellat itaque accusantium, nobis officia asperiores placeat voluptatum reiciendis illo eligendi eaque qui alias.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur consequatur incidunt cumque praesentium perspiciatis, quidem laudantium dolorum excepturi, quia ea labore facere, debitis vel magnam ipsam impedit explicabo sapiente saepe!
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero tempora pariatur quibusdam odio nulla dolore, magni odit incidunt deserunt corrupti nam tempore, velit impedit earum, ullam fuga delectus reiciendis. Asperiores!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nemo eveniet nulla vitae, fuga neque quas aperiam officiis assumenda, quisquam vel optio veritatis quia doloremque aliquid sunt voluptate eligendi beatae!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eos quos expedita quisquam laborum officia ipsam, quo amet! Vero quae est animi enim eligendi dolores tempore, aspernatur dignissimos nihil. Architecto.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis sunt eum esse, rem soluta odio, repellat dolorem aperiam quae quo alias ducimus consequatur possimus pariatur. Temporibus nisi at exercitationem.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit, corrupti quam recusandae illum voluptas error consequatur. Officiis laudantium ipsum debitis possimus ipsam odio doloremque, illum provident. Alias, illo aliquam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id saepe sint harum doloremque aliquid aperiam quisquam at ullam quibusdam? Soluta, similique facilis. At totam officiis tenetur tempore quos sint natus?
+                <div className="wd-float-done"></div>
+            </div>
+            <div id="wd-float-divs">
+                <h2>Float 2</h2>
+                <div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                        Yellow </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                        Blue </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                        Red </div>
+                    <img className="wd-float-right"
+                        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                    <div className="wd-float-done"></div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    );
+}
