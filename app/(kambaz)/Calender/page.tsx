1c71148d266474bf81e2efe29f8455c04b73e5a6
Vishwapujara <vishwapujara.08@gmail.com>
2025-10-04 12:09:24 -0400

    with calnder and inbox

---
+++ app/(kambaz)/Calender/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+export default function Calender() {
+ return (
+   <div id="wd-calender">
+    <h1>Calender</h1>
+    </div>
+);}
---
+++ app/(kambaz)/Inbox/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+export default function Inbox() {
+ return (
+   <div id="wd-inbox">
+    <h1>Inbox</h1>
+    </div>
+);}
