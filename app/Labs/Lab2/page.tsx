37d9c1da4d243ac24ba5c69d0b35c1208c6013c4
Vishwapujara <vishwapujara.08@gmail.com>
2025-10-05 08:59:53 -0400

    lab 2.1 part 3

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -17,10 +17,9 @@ export default function Lab2() {
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p id="wd-css-style-attribute">
-        Style attribute allows configuring look and feel
-        right on the element. Although it's very convenient
-        it is considered bad practice and you should avoid
-        using the style attribute</p>
+        Style attribute allows configuring look and feel right on the element.
+        Although it&apos;s very convenient it is considered bad practice and you
+        should avoid using the style attribute</p>
       <div id="wd-css-id-selectors">
         <h3>ID selectors</h3>
         <p id="wd-id-selector-1">
@@ -28,16 +27,16 @@ export default function Lab2() {
           elements of the same name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and
-          feel
+          Here&apos;s another paragraph using a different ID and a different
+          look and feel
         </p>
       </div>
       <div id="wd-css-class-selectors">
         <h3>Class selectors</h3>
 
         <p className="wd-class-selector">
-          Instead of using IDs to refer to elements, you can use an element's CLASS attribute
-        </p>
+Instead of using IDs to refer to elements, you can use an element&apos;s
+          CLASS attribute        </p>
 
         <h4 className="wd-class-selector">
           This heading has same style as paragraph above
@@ -50,8 +49,7 @@ export default function Lab2() {
             Selectors can be combined to refer elements in particular
             places in the document
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as
-              <br />
+This paragraph&apos;s red background is referenced as              <br />
               .selector-2 .selector3<br />
               meaning the descendant of some ancestor.<br />
               <span className="wd-selector-4">
