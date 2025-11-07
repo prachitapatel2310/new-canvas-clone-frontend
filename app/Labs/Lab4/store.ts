6261055b3fcb88b4089180057c1e6269022393d5
Vishwapujara <vishwapujara.08@gmail.com>
2025-11-07 18:51:20 -0500

    assignmet 4(8)

--- app/Labs/Lab4/store.ts
+++ app/Labs/Lab4/store.ts
@@ -4,15 +4,20 @@ import counterReducer from "./ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "./ReduxExamples/AddRedux/addReducer";
 import todosReducer from "./ReduxExamples/todos/todosReducer";
 
-const store = configureStore({
-  reducer: {
-    helloReducer,
-    counterReducer,
-    addReducer,
-    todosReducer,
-  },
-});
-// debug: log initial store state when module loads
-try { console.log("Lab4 store created, initial state:", store.getState()); } catch (e) {}
-export type RootState = ReturnType<typeof store.getState>;
-export default store;
+// Factory to create a fresh store instance for Lab 4
+export const makeLab4Store = () => {
+  return configureStore({
+    reducer: {
+      helloReducer,
+      counterReducer,
+      addReducer,
+      todosReducer,
+    },
+  });
+};
+
+// Infer the type of makeStore
+export type AppLab4Store = ReturnType<typeof makeLab4Store>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppLab4Store['getState']>;
+export type AppDispatch = AppLab4Store['dispatch'];
